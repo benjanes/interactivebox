a83a27c51fbf876415b810120f5167aadca734ed
Ben Janes <brjanes@gmail.com>
2014-07-08 14:38:08 -0400

    vertical and horizontal box animations enabled. toggled manually in js. need to fix vertical animations

--- interactivebox.js
+++ interactivebox.js
@@ -1,10 +1,13 @@
 $('document').ready(function() {
+
+$box = $('.box');
+
 	
-var box_initiate = function (w, h){	
+var box_horizontal = function (w, h){	
 	$('#box-container').css({width: w+'px', height: h+'px'});
-	$('#left').css({width: (w/4)+'px', height: (h)+'px'});
-	$('#right').css({width: (w/4)+'px', height: (h)+'px'});
-	$box = $('.box');
+	$('#left').css({width: (w/4)+'px', height: h+'px'});
+	$('#right').css({width: (w/4)+'px', height: h+'px'});
+	
 	// initial placement of all boxes
 	$box.css({top: (h/2)+'px', left: '0px', height: '0px', width: '0px'})
 	// initial placements of visible boxes
@@ -23,6 +26,35 @@ var box_initiate = function (w, h){
 	e = 4;
 	f = 5;
 	left = 6;
+	animateInterval = 500; // speed of the animation steps
+	itemInterval = 1000; // delay interval on the loop
+	
+}
+
+var box_vertical = function (w, h){
+	$('#box-container').css({width: w+'px', height: h+'px'});
+	$('#top').css({width: w+'px', height: (h/4)+'px'});
+	$('#bottom').css({width: w+'px', height: (h/4)+'px'});
+	$box = $('.box');
+
+	// initial placement of all boxes
+	$box.css({top: '0px', left: (w/2)+'px', height: '0px', width: '0px'})
+	// initial placements of visible boxes
+	$box.eq(4).css({left: (7*w/16)+'px',top: (h/32)+'px',height: (h/16)+'px',width: (w/8)+'px'});
+	$box.eq(3).css({left: (3*w/8)+'px',top: (h/8)+'px',height: (h/8)+'px',width: (w/4)+'px'});
+	$box.eq(2).css({left: (w/4)+'px',top: (3*h/8)+'px',height: (h/4)+'px',width: (w/2)+'px'});
+	$box.eq(1).css({left: (3*w/8)+'px',top: (3*h/4)+'px',height: (h/8)+'px',width: (w/4)+'px'});
+	$box.eq(0).css({left: (7*w/16)+'px',top: (29*h/32)+'px',height: (h/16)+'px',width: (w/8)+'px'});
+
+	numberOfItems = $('.box').length;
+	top = numberOfItems-1;
+	a = 0;
+	b = 1;
+	c = 2;
+	d = 3;
+	e = 4;
+	f = 5;
+	bottom = 6;
 	animateInterval = 500; // speed of the animation steps
 	itemInterval = 1000; // delay interval on the loop
 }
@@ -111,7 +143,93 @@ var counterclockwise = function() {
 	}else{
 	    right++;					
 	}
-}; // END of counterclockwise() definition
+} // END of counterclockwise() definition
+
+// defines steps in counterclockwise_vert motion (#top hover)	
+var counterclockwise_vert = function() {
+	$box.eq(a).stop().animate({
+		top: h+'px',
+		left: (w/2)+'px',
+		width: '0px',
+		height: '0px'
+	}, animateInterval);
+	$box.eq(b).stop().animate({
+		left: (7*w/16)+'px',
+		top: (29*h/32)+'px',
+		height: (h/16)+'px',
+		width: (w/8)+'px'
+	}, animateInterval);
+	$box.eq(c).stop().animate({
+		left: (3*w/8)+'px',
+		top: (3*h/4)+'px',
+		height: (h/8)+'px',
+		width: (w/4)+'px'
+	}, animateInterval);
+	$box.eq(d).stop().animate({
+		left: (w/4)+'px',
+		top: (3*h/8)+'px',
+		height: (h/4)+'px',
+		width: (w/2)+'px'
+	}, animateInterval);
+	$box.eq(e).stop().animate({
+		left: (3*w/8)+'px',
+		top: (h/8)+'px',
+		height: (h/8)+'px',
+		width: (w/4)+'px'
+	}, animateInterval);
+	$box.eq(f).stop().animate({
+		left: (7*w/16)+'px',
+		top: (h/32)+'px',
+		height: (h/16)+'px',
+		width: (w/8)+'px'
+	}, animateInterval);
+	$box.eq(bottom).stop().animate({
+		left: (w/2)+'px',
+		top: '0px',
+		width: '0px',
+		height: '0px'
+	}, animateInterval);
+	if(a == numberOfItems -1){
+	    a = 0;	
+	}else{
+	    a++;					
+	}
+	if(b == numberOfItems -1){
+	    b = 0;	
+	}else{
+	    b++;					
+	}
+	if(c == numberOfItems -1){
+	    c = 0;	
+	}else{
+	    c++;					
+	}
+	if(d == numberOfItems -1){
+	    d = 0;	
+	}else{
+	    d++;					
+	}
+	if(e == numberOfItems -1){
+	    e = 0;	
+	}else{
+	    e++;					
+	}
+	if(f == numberOfItems -1){
+	    f = 0;	
+	}else{
+	    f++;					
+	}
+	if(top == numberOfItems -1){
+	    top = 0;	
+	}else{
+	    top++;					
+	}
+	if(bottom == numberOfItems -1){
+	    bottom = 0;	
+	}else{
+	    bottom++;					
+	}
+} // END of counterclockwise_vert() definition
 
 // defines steps in clockwise motion
 var clockwise = function() {
@@ -203,7 +321,99 @@ var clockwise = function() {
 	}else{
 	    left--;					
 	}
-}; // END of clockwise() definition
+} // END of clockwise() definition
+
+// defines steps in clockwise_vert motion (#bottom hover)
+var clockwise_vert = function() {
+	$box.eq(top).stop().animate({
+		top: h+'px',
+		left: (w/2)+'px',
+		width: '0px',
+		height: '0px'
+	}, 0)
+	.stop().animate({
+		left: (7*w/16)+'px',
+		top: (29*h/32)+'px',
+		height: (h/16)+'px',
+		width: (w/8)+'px'
+	}, animateInterval);
+	$box.eq(a).stop().animate({
+		left: (3*w/8)+'px',
+		top: (3*h/4)+'px',
+		height: (h/8)+'px',
+		width: (w/4)+'px'
+	}, animateInterval);
+	$box.eq(b).stop().animate({
+		left: (w/4)+'px',
+		top: (3*h/8)+'px',
+		height: (h/4)+'px',
+		width: (w/2)+'px'
+	}, animateInterval);
+	$box.eq(c).stop().animate({
+		left: (3*w/8)+'px',
+		top: (h/8)+'px',
+		height: (h/8)+'px',
+		width: (w/4)+'px'
+	}, animateInterval);
+	$box.eq(d).stop().animate({
+		left: (7*w/16)+'px',
+		top: (h/32)+'px',
+		height: (h/16)+'px',
+		width: (w/8)+'px'
+	}, animateInterval);
+	$box.eq(e).stop().animate({
+		left: (w/2)+'px',
+		top: '0px',
+		width: '0px',
+		height: '0px'
+	}, animateInterval);
+	$box.eq(f).stop().animate({
+		top: h+'px',
+		left: (w/2)+'px',
+		width: '0px',
+		height: '0px'
+	}, animateInterval);
+	if(a == 0){
+	    a = numberOfItems -1;	
+	}else{
+	    a--;					
+	}
+	if(b == 0){
+	    b = numberOfItems -1;	
+	}else{
+	    b--;					
+	}
+	if(c == 0){
+	    c = numberOfItems -1;	
+	}else{
+	    c--;					
+	}
+	if(d == 0){
+	    d = numberOfItems -1;	
+	}else{
+	    d--;					
+	}
+	if(e == 0){
+	    e = numberOfItems -1;	
+	}else{
+	    e--;					
+	}
+	if(f == 0){
+	    f = numberOfItems -1;	
+	}else{
+	    f--;					
+	}
+	if(bottom == 0){
+	    bottom = numberOfItems -1;	
+	}else{
+	    bottom--;					
+	}
+	if(top == 0){
+	    top = numberOfItems -1;	
+	}else{
+	    top--;					
+	}
+}; // END of clockwise_vert() definition
 
 // on hover on left side of container, start loop moving counterclockwise
 $('#left').hover(function() {
@@ -219,7 +429,23 @@ $('#right').hover(function() {
 	loop_on_right = setInterval(clockwise, itemInterval);
 }, function() {
 	clearInterval(loop_on_right);
-});			
+});
+
+// on hover on top side of container, start loop moving counterclockwise_vert
+$('#top').hover(function() {
+	counterclockwise_vert(); // bypass itemInterval delay on initial mouseover
+	loop_on_top = setInterval(counterclockwise_vert, itemInterval);
+}, function() {
+	clearInterval(loop_on_top);
+});
+
+// on hover on bottom side of container, start loop moving clockwise_vert			
+$('#bottom').hover(function() {
+	clockwise_vert(); // bypass itemInterval delay on initial mouseover
+	loop_on_bottom = setInterval(clockwise_vert, itemInterval);
+}, function() {
+	clearInterval(loop_on_bottom);
+});
 
 
 
@@ -233,7 +459,7 @@ $('#submit').click(function(e){
 		$('.box').css({'-moz-border-radius': '0px', '-webkit-border-radius': '0px', 'border-radius': '0px'});
 	}
 
-	box_initiate(w, h);
+	box_vertical(w, h);
  	e.preventDefault();
 });
  
